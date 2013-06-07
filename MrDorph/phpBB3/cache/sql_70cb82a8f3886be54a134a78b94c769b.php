bcb4495af6a15c22453adbc5b0be8db9e9999b98
olls <faircliff.oliver@gmail.com>
2013-06-07 23:00:17 +0100

    MrDorph - Changed phpbb domain setting

--- MrDorph/phpBB3/cache/sql_70cb82a8f3886be54a134a78b94c769b.php
+++ MrDorph/phpBB3/cache/sql_70cb82a8f3886be54a134a78b94c769b.php
@@ -1,5 +1,5 @@
 <?php exit; ?>
-1370636097
+1370641501
 SELECT s.style_id, c.theme_id, c.theme_data, c.theme_path, c.theme_name, c.theme_mtime, i.*, t.template_path FROM phpbb_styles s, phpbb_styles_template t, phpbb_styles_theme c, phpbb_styles_imageset i WHERE s.style_id = 8 AND t.template_id = s.template_id AND c.theme_id = s.theme_id AND i.imageset_id = s.imageset_id
 71277
 a:1:{i:0;a:11:{s:8:"style_id";s:1:"8";s:8:"theme_id";s:1:"7";s:10:"theme_data";s:70911:"/*  phpBB3 Style Sheet
