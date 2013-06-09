81f85c2138f4349f00bbfda1d494713bb75f8030
olls <faircliff.oliver@gmail.com>
2013-06-09 21:36:09 +0100

    MrDorph - Added Header/Footer

---
+++ MrDorph/site_parts/header.php
@@ -0,0 +1,14 @@
+<header>
+	<h1>ENGLISH ARMOURED BRIGADE</h1>
+</header>
+<nav>
+	<ul>
+		<li><a href="index.php">Welcome</a></li>
+		<li><a href="info.php">Info</a></li>
+		<li><a href="news.php">News</a></li>
+		<li><a href="gallery.php">Gallery</a></li>
+		<li><a href="forum.php">Forum</a></li>
+		<li><a href="downloads.php">Downloads</a></li>
+		<li><a href="contact.php">Contact</a></li>
+	</ul>
+</nav>
