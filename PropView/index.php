f2a03c830c7cf6fb469490656f6cb0b496add5d2
grit96 <geraintwhite@gmail.com>
2013-06-16 12:30:30 +0100

    MrDorph - removed database credentials

--- PropView/index.php
+++ PropView/index.php
@@ -15,17 +15,17 @@
 						<a href="index.php"><img src="files/logo.png" id="logo" alt="PropView Logo"></a>
 						<figcaption>Virtual Property Supervision</figcaption>
 					</figure>
-					<form action="user.php" method="post" class="pure-form pure-form-stacked login">
+					<form action="<? echo (isset($_GET['admin']) ? 'admin.php' : 'user.php'); ?>" method="post" class="pure-form pure-form-stacked login">
 						<fieldset>
 							<div>
 								<label for="username-input">Username:</label>
-								<input type="text" id="username-input" placeholder="Username" required>
+								<input type="text" id="username-input" name="username" placeholder="Username" required>
 							</div>
 							<div>
 								<label for="password-input">Password:</label>
-								<input type="password" id="password-input" placeholder="Password" required>
+								<input type="password" id="password-input" name="password" placeholder="Password" required>
 							</div>
-							<button type="submit" class="pure-button">Login</button>
+							<button type="submit" name="login" class="pure-button">Login</button>
 						</fieldset>
 					</form>
 				</div>
